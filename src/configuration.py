a07cc5c6b3e18762f94066e983e74ea0f9334636
Farid Ch <chouchanefarid@gmail.com>
2026-03-15 00:15:27 +0100

    travil sur la branch intor classes

--- src/configuration.py
+++ src/configuration.py
@@ -1,40 +1,41 @@
 import requests
+import json
 
+# ----------------------------------------------------------------------------------------
 class Configuration:
 
     def __init__(self, input_config):
-        self.db_path = input_config["db_path"]
-        self.fichier_sql = input_config["fichier_sql"]
-        self.json_path = input_config["json_path"]
-        self.type_api = input_config["type_api"]
-        self.dataset = input_config["dataset"]
-        self.sql_creation = input_config["sql_creation"]
-        self.nom_table = input_config["nom_table"]
+        self.input_config = input_config
+
 
     def telecharger(self):
-        if self.type_api == "conso_gouv":
-            return self.telecharger_data_gouv()
-        elif self.type_api == "economie_gouv":
-            return self.telecharger_donnees_economie_gouv()
-        else:
-            raise ValueError(f"La valeur de type_api = {self.type_api} n'est pas connue")
+        raise NotImplementedError()
 
-    def telecharger_data_gouv(self):
-        print("Télécharger les données")
+# ----------------------------------------------------------------------------------------
+class BeautifulPrinter():
 
-        toutes_les_data = []
-        url = f"https://tabular-api.data.gouv.fr/api/resources/{self.dataset}/data/?Date__exact=2025-12-31"
+    def __init__(self, input_config):
+        self.input_config = input_config
 
-        while url:
-            r = requests.get(url)
-            r.raise_for_status()
-            data = r.json()
-            toutes_les_data += data["data"]
-            url = data["links"].get("next")
+    def print(self):
+        print(json.dumps(self.input_config, indent = 4))
 
-        return toutes_les_data
+# ---------------------------------------------------------------------------------------
+class EconomieGouvConfiguration(Configuration, BeautifulPrinter):
 
-    def telecharger_donnees_economie_gouv(self):
+    def __init__(self, input_config):
+        super().__init__(input_config)
+        self.db_path = self.input_config["db_path"]
+        self.fichier_sql = self.input_config["fichier_sql"]
+        self.json_path = self.input_config["json_path"]
+        self.type_api = self.input_config["type_api"]
+        self.dataset = self.input_config["dataset"]
+        self.sql_creation =self.input_config["sql_creation"]
+        self.nom_table = self.input_config["nom_table"]
+        self.url = f"https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/{self.dataset}/records?select=id%2Clatitude%2Clongitude%2Ccp%2Cadresse%2Cville%2Cservices%2Cgazole_prix%2Cgazole_maj%2Choraires%2Csp95_maj%2Csp95_prix%2Csp98_maj%2Csp98_prix&limit={{limit}}&offset={{offset}}"
+
+
+    def telecharger(self):
         print("Télécharger les données")
 
         toutes_les_data = []
@@ -42,10 +43,8 @@ class Configuration:
         step = 100
         offset = 0
 
-        url = f"https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/{self.dataset}/records?select=id%2Clatitude%2Clongitude%2Ccp%2Cadresse%2Cville%2Cservices%2Cgazole_prix%2Cgazole_maj%2Choraires%2Csp95_maj%2Csp95_prix%2Csp98_maj%2Csp98_prix&limit={{limit}}&offset={{offset}}"
-
         while True:
-            r = requests.get(url.format(limit=step, offset=offset))
+            r = requests.get(self.url.format(limit=step, offset=offset))
             r.raise_for_status()
             data = r.json()
 
@@ -60,3 +59,32 @@ class Configuration:
                 break
 
         return toutes_les_data
+
+# ----------------------------------------------------------------------------------------
+class DataGouvConfiguration(Configuration):
+
+    def __init__(self, input_config):
+        super().__init__(input_config)
+        self.db_path = self.input_config["db_path"]
+        self.fichier_sql = self.input_config["fichier_sql"]
+        self.json_path = self.input_config["json_path"]
+        self.type_api = self.input_config["type_api"]
+        self.dataset = self.input_config["dataset"]
+        self.sql_creation =self.input_config["sql_creation"]
+        self.nom_table = self.input_config["nom_table"]
+        self.url = f"https://tabular-api.data.gouv.fr/api/resources/{self.dataset}/data/?Date__exact=2025-12-31"
+
+    def telecharger(self):
+        print("Télécharger les données")
+        toutes_les_data = []
+        url = self.url
+        while url:
+            r = requests.get(url)
+            r.raise_for_status()
+            data = r.json()
+            toutes_les_data += data["data"]
+            url = data["links"].get("next")
+
+        return toutes_les_data
+
+# ----------------------------------------------------------------------------------------
--- src/utils/config_reader.py
+++ src/utils/config_reader.py
@@ -1,5 +1,5 @@
 import json
-from configuration import Configuration
+from configuration import EconomieGouvConfiguration, DataGouvConfiguration
 
 def read_configuration():
     out = []
@@ -9,8 +9,12 @@ def read_configuration():
 
     for config in configuration:
         config["sql_creation"] = sql_finder(config["fichier_sql"])
-        out.append(Configuration(config))
-
+        if config["type_api"] == "economie_gouv":
+            out.append(EconomieGouvConfiguration(config))
+        elif config["type_api"] == "conso_gouv":
+            out.append(DataGouvConfiguration(config))
+        else:
+            raise ValueError(f"La clé type_api = {config['type_api']} n 'est pas connue")
     return out
 
 
